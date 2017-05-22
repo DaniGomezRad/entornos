80194e8625d7bc46a894eccd162291ba1631a99b
dam <dam@AFEQUIPO04>
2017-05-22 17:26:32 +0200

    cambio 1

--- DaniEnt/src/Prueba.java
+++ DaniEnt/src/Prueba.java
@@ -3,7 +3,7 @@ public class Prueba {
 
 	public static void main(String[] args) {
 		int a=3,b=1;
-		System.out.println("a-b es: "+(a-b));
+		System.out.println("a-b es: "+(a-b));//comentario
 
 	}
 
